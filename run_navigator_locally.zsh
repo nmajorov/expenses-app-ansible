d0452d8dac94d0ec96e7f0894a94a0bc7e3391c6
Nikolaj Majorov <nikolaj@majorov.biz>
2022-08-19 17:29:44 +0200

    add wildcard domain definition

--- run_navigator_locally.zsh
+++ run_navigator_locally.zsh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
-#
-ansible-navigator  run -v   site.yaml -m stdout --eev $HOME/.kube:/home/runner/.kube --senv KUBECONFIG="/home/runner/.kube/config"
+SCRIPT_PATH=${0:a}
+ansible-navigator  run -v   site.yaml -m stdout --eev $HOME/.kube:/home/runner/.kube \
+     \
+    --senv KUBECONFIG="/home/runner/.kube/config"
